a881120b13419fa158766179a14650e3f2f724fe
ZahidHasan000 <h.zahid007.zh59@gmail.com>
2023-08-08 17:36:16 +0600

    Zahid Hasan
    i have modify my Log in feature code

--- src/components/Sign-in/Login.jsx
+++ src/components/Sign-in/Login.jsx
@@ -1,59 +1,143 @@
-import React, { useState } from 'react'
-import { Container, Row, Col, Form, Alert, Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import * as Yup from 'yup';
+import { useState } from 'react'; // Remove useEffect
+import { useFormik, Form, FormikProvider } from 'formik';
+import { useNavigate } from 'react-router-dom';
 
-// import Iconify from './Iconify';
+import { styled } from '@mui/material/styles';
+import {
+    Card,
+    Container,
+    Typography,
+    Stack,
+    TextField,
+    IconButton,
+    InputAdornment
+} from '@mui/material';
+
+import { LoadingButton } from '@mui/lab';
+import Iconify from '../../components/Iconify';
 
 import { useLoginMutation } from "../../services/appApi";
 
-function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+const SectionStyle = styled(Card)(({ theme }) => ({
+    width: '100%',
+    maxWidth: 464,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    margin: theme.spacing(2, 0, 2, 2),
+}));
 
-    const [login, { isError, isLoading, error }] = useLoginMutation();
-    function handleLogin(e) {
-        e.preventDefault();
-        login({ email, password });
-    }
+const ContentStyle = styled('div')(({ theme }) => ({
+    maxWidth: 800,
+    margin: 'auto',
+    minHeight: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    padding: theme.spacing(12, 0),
+}));
+
+export default function Login() {
+    const navigate = useNavigate();
+    const [showPassword, setShowPassword] = useState(false);
+    const [loginError, setLoginError] = useState(null);
+    const [login, { isLoading }] = useLoginMutation();
+
+    const UserSchema = Yup.object().shape({
+        email: Yup.string().email('Email must be a valid email address').required('Email is required'),
+        password: Yup.string().required('Password is required')
+            .min(8, "Password must have at least 8 characters")
+            .matches(/[0-9]/, "Must have a number")
+            .matches(/[a-z]/, "Must have atleast one lowercase")
+            .matches(/[A-Z]/, "Must have atleast one Uppercase")
+            .matches(/[!@#$%^&*]/, "Must have atleast one special character"),
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        validationSchema: UserSchema,
+        onSubmit: async (values) => {
+            try {
+                const response = await login({ email: values.email, password: values.password });
+                // Handle success, e.g., redirect to a dashboard page
+                console.log('Login successful:', response.data);
+                navigate('/dashboard'); // Replace '/dashboard' with your actual dashboard route
+            } catch (error) {
+                // Handle error, display error message, etc.
+                console.error('Login error:', error);
+                setLoginError("Invalid email or password");
+            }
+        },
+    });
+
+    const {
+        errors,
+        touched,
+        handleSubmit,
+        isSubmitting,
+        getFieldProps
+    } = formik;
+
+    // ... rest of the code ...
 
     return (
         <Container>
-            <Row>
-                <Col md={6} className='login__form--container'>
-                    {/* <Form style={{ width: '100%' }}> */}
-
-                    <Form style={{ width: "100%" }} onSubmit={handleLogin}>
-                        <h1>Login to your account</h1>
-
-                        {isError && <Alert variant="danger">{error.data}</Alert>}
-
-                        <Form.Group>
-                            <Form.Label>Email Address</Form.Label>
-                            <Form.Control type='email' placeholder='Enter email' value={email} required onChange={(e) => setEmail(e.target.value)} />
-                        </Form.Group>
-
-                        <Form.Group>
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type='password' placeholder='Enter password' value={password} required onChange={(e) => setPassword(e.target.value)} />
-                        </Form.Group>
-
-                        <Form.Group>
-                            <Button type="submit" disabled={isLoading}>
-                                Login
-                            </Button>
-                        </Form.Group>
-                        <p>
-                            Don't have an account? <Link to='/signup'>Create account</Link>
-                        </p>
-                    </Form>
-                </Col>
-                <Col md={6} className='login__image--container'>
-
-                </Col>
-            </Row>
+            <Card>
+                <Stack spacing={3} p={4}>
+                    <Typography variant="h4" gutterBottom>
+                        Login
+                    </Typography>
+                    <TextField
+                        fullWidth
+                        autoComplete="username"
+                        type="email"
+                        label="Email address"
+                        {...getFieldProps('email')}
+                        error={Boolean(touched.email && errors.email)}
+                        helperText={touched.email && errors.email}
+                    />
+                    <TextField
+                        fullWidth
+                        autoComplete="current-password"
+                        type={showPassword ? 'text' : 'password'}
+                        label="Password"
+                        {...getFieldProps('password')}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton edge="end" onClick={() => setShowPassword((prev) => !prev)}>
+                                        <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                        }}
+                        error={Boolean(touched.password && errors.password)}
+                        helperText={touched.password && errors.password}
+                    />
+                    {loginError && (
+                        <Typography color="error" variant="body2">
+                            {loginError}
+                        </Typography>
+                    )}
+                    <LoadingButton
+                        fullWidth
+                        size="large"
+                        type="submit"
+                        variant="contained"
+                        loading={isLoading || isSubmitting}
+                        onClick={handleSubmit}
+                    >
+                        Login
+                    </LoadingButton>
+                </Stack>
+            </Card>
         </Container>
-    )
-}
+    );
+};
+// export default Login;
 
-export default Login
 
