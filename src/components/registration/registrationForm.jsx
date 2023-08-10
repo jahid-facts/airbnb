7f098071c1a84768748d236a59231c52226dd6e0
SyedAshrafAsif <syedashrafasif@gmail.com>
2023-08-11 00:52:29 +0600

    added DateTime Picker component

--- src/components/registration/registrationForm.jsx
+++ src/components/registration/registrationForm.jsx
@@ -1,152 +1,247 @@
-import React from 'react';
-import { useFormik } from 'formik';
+import dayjs from "dayjs";
 import * as Yup from 'yup';
+import { useState } from 'react';
+import { useFormik, Form, FormikProvider } from 'formik';
+import { styled } from '@mui/material/styles';
 import {
-  TextField,
-  Button,
-  FormControl,
-  FormLabel,
-  RadioGroup,
-  FormControlLabel,
-  Radio,
+    Select,
+    Link,
+    Container,
+    Typography,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Stack,
+    TextField,
+    IconButton,
+    InputAdornment
 } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import Iconify from '../Iconify';
+import DateTimePicker from '../registration/DateTimePicker';
+//import UserDataService from "../../services/UserDataService";
 
-const validationSchema = Yup.object({
-  firstName: Yup.string().required('First name is required'),
-  lastName: Yup.string().required('Last name is required'),
-  gender: Yup.string().required('Please select a gender'),
-  dob: Yup.date().required('Date of birth is required'),
-  email: Yup.string().email('Invalid email').required('Email is required'),
-  password: Yup.string()
-    .min(8, 'Password must be at least 8 characters')
-    .required('Password is required'),
-  confirmPassword: Yup.string()
-    .oneOf([Yup.ref('password'), null], 'Passwords must match')
-    .required('Confirm password is required'),
-});
+const ContentStyle = styled('div')(({ theme }) => ({
+    maxWidth: 800,
+    margin: 'auto',
+    minHeight: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    padding: theme.spacing(12, 0),
+}));
+export default function RegistrationForm() {
+    const [showPassword, setShowPassword] = useState(false);
+    const minDob = dayjs().subtract(18, "year").format("YYYY-MM-DD");
+    const [role, setRole] = useState('renter');
+    const handleRoleChange = (event) => {
+        setRole(event.target.value);
+    };
 
-const registrationForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      firstName: '',
-      lastName: '',
-      gender: '',
-      dob: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
-    },
-    validationSchema: validationSchema,
-    onSubmit: (values) => {
-      // Handle form submission logic here
-      console.log(values);
-    },
-  });
+    const UserSchema = Yup.object().shape({
+        name: Yup.string().min(5, 'Too Short!').max(50, 'Too Long!').required('First name required'),
+        email: Yup.string().email('Email must be a valid email address').required('Email is required'),
+        password: Yup.string().required('Password is required')
+            .min(8, "Password must have at least 8 characters")
+            .matches(/[0-9]/, "Must have a number")
+            .matches(/[a-z]/, "Must have atleast one lowercase")
+            .matches(/[A-Z]/, "Must have atleast one Uppercase")
+            .matches(/[!@#$%^&*]/, "Must have atleast one special character"),
+        confirmPassword: Yup.string().required("Please re-type your password").oneOf([Yup.ref("password"), null], "Passwords does not match"),
+        dob: Yup.date().max(minDob, "Must be more than 18 years of age")
+    });
 
-  const { values, errors, touched, isSubmitting } = formik;
+    const formik = useFormik({
+        initialValues: {
+            role: 'renter',
+            name: '',
+            gender: '',
+            dob: minDob,
+            nationalId: '',
+            email: '',
+            phoneNumber: '',
+            password: '',
+            confirmPassword: '',
+        },
+        validationSchema: UserSchema,
+        /*onSubmit: (values) => {
+            console.log(values);
+            UserDataService.create(values)
+                .then((res) => {
+                    console.log('Created User successfully.');
+                    navigate('/dashboard', { replace: true });
+                }).catch(e => {
+                    setSubmitting(false);
+                    formik.setFieldError('email', 'You are already registered');
+                    console.log(e);
+                });
+        }*/
+    });
+    const { values, errors, touched, handleChange, handleSubmit, isSubmitting, getFieldProps } = formik;
 
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      <TextField
-        fullWidth
-        id="firstName"
-        name="firstName"
-        label="First Name"
-        value={formik.values.firstName}
-        onChange={formik.handleChange}
-        error={formik.touched.firstName && Boolean(formik.errors.firstName)}
-        helperText={formik.touched.firstName && formik.errors.firstName}
-      />
+    return (
+        <Container>
+            <ContentStyle>
+                <Typography variant="h4" gutterBottom>
+                    User Registration
+                </Typography>
+                <FormikProvider value={formik}>
+                    <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+                        <Stack spacing={3}>
+                            {/* Role Field */}
+                            <FormControl fullWidth>
+                                <InputLabel
+                                    id="role-select-label" margin="dense" variant="outlined" {...getFieldProps('role')} value={role}
+                                    error={Boolean(touched.role && errors.role)}
+                                >Select Role</InputLabel>
+                                <Select
+                                    labelId="role-select-label"
+                                    id="role-select"
+                                    {...getFieldProps('role')}
+                                    value={role}
+                                    label="Select Role"
+                                    onChange={handleRoleChange}
+                                >
+                                    <MenuItem value={"host"}>Host</MenuItem>
+                                    <MenuItem value={"renter"}>Renter</MenuItem>
+                                </Select>
+                            </FormControl>
 
-      <TextField
-        fullWidth
-        id="lastName"
-        name="lastName"
-        label="Last Name"
-        value={formik.values.lastName}
-        onChange={formik.handleChange}
-        error={formik.touched.lastName && Boolean(formik.errors.lastName)}
-        helperText={formik.touched.lastName && formik.errors.lastName}
-      />
+                            {/* Name Field */}
+                            <TextField
+                                fullWidth
+                                autoComplete="fullname"
+                                type="text"
+                                label="Full Name"
+                                {...getFieldProps('name')}
+                                error={Boolean(touched.name && errors.name)}
+                                helperText={touched.name && errors.name}
+                            />
 
-      <FormControl component="fieldset">
-        <FormLabel component="legend">Gender</FormLabel>
-        <RadioGroup
-          id="gender"
-          name="gender"
-          value={formik.values.gender}
-          onChange={formik.handleChange}
-        >
-          <FormControlLabel value="male" control={<Radio />} label="Male" />
-          <FormControlLabel value="female" control={<Radio />} label="Female" />
-          <FormControlLabel value="other" control={<Radio />} label="Other" />
-        </RadioGroup>
-        {formik.touched.gender && formik.errors.gender && (
-          <div>{formik.errors.gender}</div>
-        )}
-      </FormControl>
+                            {/* Gender Field */}
+                            <FormControl fullWidth>
+                                <InputLabel
+                                    id="gender-label" margin="dense" variant="outlined"
+                                >Select Gender</InputLabel>
+                                <Select
+                                    labelId="gender-label"
+                                    id="gender"
+                                    {...getFieldProps('gender')}
+                                    value={values.gender}
+                                    label="Select Gender"
+                                    onChange={handleChange}
+                                    error={Boolean(touched.gender && errors.gender)}
+                                >
+                                    <MenuItem value="Male">Male</MenuItem>
+                                    <MenuItem value="Female">Female</MenuItem>
+                                    <MenuItem value="Other">Other</MenuItem>
+                                </Select>
+                                {touched.gender && errors.gender && (
+                                    <Typography color="error">{errors.gender}</Typography>
+                                )}
+                            </FormControl>
 
-      <TextField
-        fullWidth
-        id="dob"
-        name="dob"
-        label="Date of Birth"
-        type="date"
-        value={formik.values.dob}
-        onChange={formik.handleChange}
-        InputLabelProps={{
-          shrink: true,
-        }}
-        error={formik.touched.dob && Boolean(formik.errors.dob)}
-        helperText={formik.touched.dob && formik.errors.dob}
-      />
+                            {/* Date of Birth Field */}
+                            <FormControl fullWidth>
+                                <DateTimePicker
+                                    name="dob"
+                                    label="Date of Birth"
+                                    {...getFieldProps('dob')}
+                                    error={Boolean(touched.dob && errors.dob)}
+                                />
+                                {touched.dob && errors.dob && (
+                                    <Typography color="error">{errors.dob}</Typography>
+                                )}
+                            </FormControl>
 
-      <TextField
-        fullWidth
-        id="email"
-        name="email"
-        label="Email"
-        type="email"
-        value={formik.values.email}
-        onChange={formik.handleChange}
-        error={formik.touched.email && Boolean(formik.errors.email)}
-        helperText={formik.touched.email && formik.errors.email}
-      />
+                            {/* National Identification Number Field */}
+                            <TextField
+                                fullWidth
+                                label="National Identification Number"
+                                {...getFieldProps('nationalId')}
+                                error={Boolean(touched.nationalId && errors.nationalId)}
+                                helperText={touched.nationalId && errors.nationalId}
+                            />
 
-      <TextField
-        fullWidth
-        id="password"
-        name="password"
-        label="Password"
-        type="password"
-        value={formik.values.password}
-        onChange={formik.handleChange}
-        error={formik.touched.password && Boolean(formik.errors.password)}
-        helperText={formik.touched.password && formik.errors.password}
-      />
+                            {/* Email Field */}
+                            <TextField
+                                fullWidth
+                                label="Email"
+                                type="email"
+                                {...getFieldProps('email')}
+                                error={Boolean(touched.email && errors.email)}
+                                helperText={touched.email && errors.email}
+                            />
 
-      <TextField
-        fullWidth
-        id="confirmPassword"
-        name="confirmPassword"
-        label="Confirm Password"
-        type="password"
-        value={formik.values.confirmPassword}
-        onChange={formik.handleChange}
-        error={
-          formik.touched.confirmPassword &&
-          Boolean(formik.errors.confirmPassword)
-        }
-        helperText={
-          formik.touched.confirmPassword && formik.errors.confirmPassword
-        }
-      />
+                            {/* Phone Number Field */}
+                            <TextField
+                                fullWidth
+                                label="Phone Number"
+                                {...getFieldProps('phoneNumber')}
+                                error={Boolean(touched.phoneNumber && errors.phoneNumber)}
+                                helperText={touched.phoneNumber && errors.phoneNumber}
+                            />
 
-      <LoadingButton fullWidth size="large" type="submit" variant="contained" loading={isSubmitting}>
-                                Save Details
-      </LoadingButton>
-    </form>
-  );
+                            {/* Password Field */}
+                            <TextField
+                                fullWidth
+                                autoComplete="current-password"
+                                type={showPassword ? 'text' : 'password'}
+                                label="Password"
+                                {...getFieldProps('password')}
+                                InputProps={{
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <IconButton edge="end" onClick={() => setShowPassword((prev) => !prev)}>
+                                                <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                                            </IconButton>
+                                        </InputAdornment>
+                                    ),
+                                }}
+                                error={Boolean(touched.password && errors.password)}
+                                helperText={touched.password && errors.password}
+                            />
+
+                            {/* Confirm Password Field */}
+                            <TextField
+                                fullWidth
+                                autoComplete="current-password"
+                                type={showPassword ? 'text' : 'password'}
+                                label="Confirm Password"
+                                {...getFieldProps('confirmPassword')}
+                                InputProps={{
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <IconButton edge="end" onClick={() => setShowPassword((prev) => !prev)}>
+                                                <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                                            </IconButton>
+                                        </InputAdornment>
+                                    ),
+                                }}
+                                error={Boolean(touched.confirmPassword && errors.confirmPassword)}
+                                helperText={touched.confirmPassword && errors.confirmPassword}
+                            />
+
+                            {/* Submit Button */}
+                            <LoadingButton
+                                fullWidth
+                                size="large"
+                                type="submit"
+                                variant="contained"
+                                loading={isSubmitting}
+                            >
+                                Register
+                            </LoadingButton>
+
+                            {/* "Sign in" button */}
+                            <Typography variant="body1">
+                                Already have an account?{' '}
+                                <Link href="/signin">Sign in</Link>
+                            </Typography>
+                        </Stack>
+                    </Form>
+                </FormikProvider>
+            </ContentStyle>
+        </Container>
+    );
 };
-
-export default registrationForm;
