b8c121e92372ab7df9214c02cd4494ca0d9d46d7
Bashar12345 <abul35-2002@diu.edu.bd>
2023-09-29 16:41:24 +0600

    init

--- src/pages/reservationEcheck/index.jsx
+++ src/pages/reservationEcheck/index.jsx
@@ -71,6 +71,59 @@ import {
             </Box>
           </Grid>
 
+
+
+          <Grid item xs={12} md={10} mx = {10} >
+            <Box
+              display={"flex"}
+              alignItems={"center"}
+              p={3}
+              boxShadow={theme.palette.boxShadow}
+              bgcolor={"#ffffff"}
+              borderRadius={"20px"}
+            >
+              <Box
+                p={1}
+                mr={2}
+                width={"40px"}
+                height={"40px"}
+                borderRadius={"50px"}
+                bgcolor={"#e0eeff"}
+                display={"flex"}
+                textAlign={"center"}
+                alignItems={"center"}
+                justifyContent={"center"}
+              >
+                {/* <BarChart sx={{ fontSize: "30px", color: "#2980b9" }} /> */}
+              </Box>
+
+
+
+              <Box>
+                <Typography variant="body1" fontSize={"14px"} color={"#7f7f7f"}>
+                Md. Abul Bashar
+                </Typography>
+                <Typography variant="h5" fontWeight={"bold"}>
+                 NId: 135346532
+               
+                </Typography>
+                <Typography variant="h5" fontWeight={"bold"}>
+               
+                 Rev-id: 4534
+                </Typography>
+                <Typography
+                  variant="body1"
+                  fontWeight={"600"}
+                  fontSize={"14px"}
+                  color={"#7f7f7f"}
+                >
+                  Paid: 343$
+                </Typography>
+              </Box>
+            </Box>
+          </Grid>
+
+
         </Grid>
       </Layout>
     );
