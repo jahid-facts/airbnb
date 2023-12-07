a498fde3c65a88d34a188e49910b5df8de6f677b
bashar12345 <abul35-2002@diu.edu.bd>
2023-12-07 18:23:21 +0600

    init

--- src/pages/profile/ProfileContent/GlobalModalForProfile.jsx
+++ src/pages/profile/ProfileContent/GlobalModalForProfile.jsx
@@ -1,105 +1,128 @@
-import { Box, Drawer, Grid } from '@mui/material';
-import React from 'react'
+import { Box, Drawer, Grid } from "@mui/material";
+import React, { useState } from "react";
+import { Formik, Field, Form } from "formik";
+import * as Yup from "yup";
 
-const GlobalModalForProfile = ({open, onClose}) => {
-    const closeDrawer = () => {
-        onClose();
-      };
+const GlobalModalForProfile = ({ open, onClose, typeOfForm }) => {
+  const closeDrawer = () => {
+    onClose();
+  };
 
+  const initialValues = {
+    legalName: "",
+    email: "",
+    phone: "",
+    governmentID: "",
+    addresses: [],
+  };
 
-      const handleSubmit =() =>{
+  const validationSchema = Yup.object({
+    legalName: Yup.string().required("Legal name is required"),
+    email: Yup.string()
+      .email("Email address is not valid")
+      .required("Email address is required"),
+    phone: Yup.string().required("Phone number is required"),
+    governmentID: Yup.string().required("Government ID is required"),
+  });
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    address: "",
+    city: "",
+  });
 
-      };
-      
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = () => {};
   return (
-    <div>GlobalModalForProfile
-
-
-
-<Drawer anchor="bottom" open={open} onClose={onClose}>
+    <div>
+      GlobalModalForProfile
+      <Drawer anchor="bottom" open={open} onClose={onClose}>
         <Grid container spacing={2} m={10}>
-
-        <form onSubmit={handleSubmit}>
-        <Box
-          sx={{
-            boxShadow: 1,
-            p: 2,
-            m: 1,
-            borderRadius: 2,
-          }}
-        >
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                name="Legal name"
-                label="Legal name"
-                variant="standard"
-                fullWidth
-                value={formData.legalName}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                name="Email address"
-                label="Email address"
-                type="email"
-                variant="standard"
-                fullWidth
-                value={formData.email}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                name="Phone numbers"
-                label="Phone numbers"
-                variant="standard"
-                fullWidth
-                value={formData.phone}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                name="Government ID"
-                label="Government ID"
-                variant="standard"
-                fullWidth
-                value={formData.governmentID}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                name="Address"
-                label="Address"
-                variant="standard"
-                fullWidth
-                value={formData.address}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                name="Emergency contact"
-                label="Emergency contact"
-                variant="standard"
-                fullWidth
-                value={formData.emergencyContact}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Button type="submit" variant="contained" color="primary">
-                Submit
-              </Button>
-            </Grid>
-          </Grid>
-        </Box>
-      </form>
-
-          
+          <form onSubmit={handleSubmit}>
+            <Box
+              sx={{
+                boxShadow: 1,
+                p: 2,
+                m: 1,
+                borderRadius: 2,
+              }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    name="Legal name"
+                    label="Legal name"
+                    variant="standard"
+                    fullWidth
+                    value={formData.legalName}
+                    onChange={handleChange}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    name="Email address"
+                    label="Email address"
+                    type="email"
+                    variant="standard"
+                    fullWidth
+                    value={formData.email}
+                    onChange={handleChange}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    name="Phone numbers"
+                    label="Phone numbers"
+                    variant="standard"
+                    fullWidth
+                    value={formData.phone}
+                    onChange={handleChange}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    name="Government ID"
+                    label="Government ID"
+                    variant="standard"
+                    fullWidth
+                    value={formData.governmentID}
+                    onChange={handleChange}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    name="Address"
+                    label="Address"
+                    variant="standard"
+                    fullWidth
+                    value={formData.address}
+                    onChange={handleChange}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    name="Emergency contact"
+                    label="Emergency contact"
+                    variant="standard"
+                    fullWidth
+                    value={formData.emergencyContact}
+                    onChange={handleChange}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button type="submit" variant="contained" color="primary">
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+            </Box>
+          </form>
         </Grid>
 
         {/* Close Button */}
@@ -132,7 +155,7 @@ const GlobalModalForProfile = ({open, onClose}) => {
         </Box>
       </Drawer>
     </div>
-  )
-}
+  );
+};
 
-export default GlobalModalForProfile
+export default GlobalModalForProfile;
--- src/pages/profile/ProfileContent/PersonalInfo.js
+++ src/pages/profile/ProfileContent/PersonalInfo.js
@@ -6,15 +6,7 @@ import { Box } from "@mui/material";
 import GlobalModalForProfile from "./GlobalModalForProfile"
 
 const PersonalInfo = () => {
-    const [globalModalForProfile ,setGlobalModalForProfile] = useState('');
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    address: "",
-    city: "",
-  });
+    const [globalModalForProfile ,setGlobalModalForProfile] = useState(false);
 
   const tilesPersonal = [
     "Personal details",
@@ -31,8 +23,6 @@ const PersonalInfo = () => {
   const handleTiles = (event) => {
     const tileClicked = event.target.textContent; // Get the text content of the button that was clicked
 
-    // Perform the desired action based on the clicked tile (e.g., navigate to a different page, display a modal, etc.)
-
     console.log(`Tile clicked: ${tileClicked}`); // Log the clicked tile to the console for testing purposes
 
    
@@ -41,10 +31,7 @@ const PersonalInfo = () => {
 
 
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
-  };
+
 
   const handleSubmit = (event) => {
     event.preventDefault();
