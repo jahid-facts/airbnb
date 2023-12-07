2fa2f35ce8ba0bc9e1a4b0297cded327de2f9adb
bashar12345 <abul35-2002@diu.edu.bd>
2023-12-07 17:20:43 +0600

    init

--- src/pages/profile/ProfileContent/GlobalModalForProfile.jsx
+++ src/pages/profile/ProfileContent/GlobalModalForProfile.jsx
@@ -5,6 +5,12 @@ const GlobalModalForProfile = ({open, onClose}) => {
     const closeDrawer = () => {
         onClose();
       };
+
+
+      const handleSubmit =() =>{
+
+      };
+      
   return (
     <div>GlobalModalForProfile
 
@@ -12,6 +18,87 @@ const GlobalModalForProfile = ({open, onClose}) => {
 
 <Drawer anchor="bottom" open={open} onClose={onClose}>
         <Grid container spacing={2} m={10}>
+
+        <form onSubmit={handleSubmit}>
+        <Box
+          sx={{
+            boxShadow: 1,
+            p: 2,
+            m: 1,
+            borderRadius: 2,
+          }}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="Legal name"
+                label="Legal name"
+                variant="standard"
+                fullWidth
+                value={formData.legalName}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="Email address"
+                label="Email address"
+                type="email"
+                variant="standard"
+                fullWidth
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="Phone numbers"
+                label="Phone numbers"
+                variant="standard"
+                fullWidth
+                value={formData.phone}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="Government ID"
+                label="Government ID"
+                variant="standard"
+                fullWidth
+                value={formData.governmentID}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="Address"
+                label="Address"
+                variant="standard"
+                fullWidth
+                value={formData.address}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="Emergency contact"
+                label="Emergency contact"
+                variant="standard"
+                fullWidth
+                value={formData.emergencyContact}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button type="submit" variant="contained" color="primary">
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </form>
+
           
         </Grid>
 
